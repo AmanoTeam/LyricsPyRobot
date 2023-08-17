63994f6c04916351eb644b34bca391150a56a11f
Rian Felipe Celestrino <39718272+RianFC@users.noreply.github.com>
2023-08-17 15:53:09 +0000

    Add donations

---
+++ plugins/donate.py
@@ -0,0 +1,83 @@
+from pyrogram import Client, filters
+from pyrogram.types import CallbackQuery, Update, Message
+from pyrogram.raw.functions.messages import SendMedia, SetBotPrecheckoutResults
+from pyrogram.raw.types import (
+    DataJSON,
+    InputMediaInvoice,
+    Invoice,
+    LabeledPrice,
+    MessageActionPaymentSentMe,
+    UpdateBotPrecheckoutQuery,
+    UpdateNewMessage,
+)
+
+from config import stripe_token, sudos
+from typing import Union
+
+from locales import use_chat_lang
+from pyromod.helpers import ikb
+
+@Client.on_callback_query(filters.regex(r"donate"))
+@use_chat_lang()
+async def add_donate(c: Client, q: CallbackQuery, t):
+    val = int(q.data.split("_")[1]) if "_" in q.data else 1000
+    if val >= 650:
+        key = [[
+            ("+ R$ 100,00", f"donate_{val+10000}"),
+            ("+ R$ 10,00", f"donate_{val+1000}"),
+            ("+ R$ 1,00", f"donate_{val+100}"),
+            ("+ R$ 0,50", f"donate_{val+50}")],
+           [("R$ "+str(val/100), f"donation_{val}")],
+           [("- R$ 100,00", f"donate_{val-10000}"),
+            ("- R$ 10,00", f"donate_{val-1000}"),
+            ("- R$ 1,00", f"donate_{val-100}"),
+            ("- R$ 0,50", f"donate_{val-50}"),
+        ]]
+        
+        await q.edit_message_text(t("se_val"), reply_markup=ikb(key))
+
+@Client.on_message(filters.regex(r"\/start pay"))
+@Client.on_callback_query(filters.regex(r"donation"))
+@use_chat_lang()
+async def donation(c: Client, q: Union[CallbackQuery, Message], t):
+    peer = await c.resolve_peer(q.from_user.id)
+    val = int(q.data.split("_")[1]) if isinstance(q, CallbackQuery) else int(q.text.split("_")[1])
+    key = stripe_token["TEST"] if q.from_user.id in sudos else stripe_token["LIVE"]
+    await c.invoke(
+        SendMedia(
+            peer=peer,
+            media=InputMediaInvoice(
+                title=t("donation_title"),
+                description=t("donation_subtitle"),
+                invoice=Invoice(
+                    currency="BRL",
+                    # prices needs to be a list, even for a single item
+                    prices=[LabeledPrice(label=t("donation_label"), amount=val)],
+                    test=True if q.from_user.id in sudos else False,
+                ),
+                payload=b"payment",
+                provider=key,
+                provider_data=DataJSON(data=r"{}"),
+                start_param=f"pay_{val}",
+            ),
+            message="",
+            random_id=c.rnd_id(),
+        )
+    )
+    return
+
+@Client.on_raw_update(group=10)
+async def raw_update(c: Client, update: Update, users: dict, chats: dict):
+    if isinstance(update, UpdateBotPrecheckoutQuery):
+        # This is to tell Telegram that everything is okay with this order.
+        await c.invoke(SetBotPrecheckoutResults(query_id=update.query_id, success=True))
+
+    if (
+        isinstance(update, UpdateNewMessage)
+        and hasattr(update.message, "action")
+        and isinstance(update.message.action, MessageActionPaymentSentMe)
+    ):
+        # Sending a message confirming the order (additional to TGs service message)
+        user = users[update.message.peer_id.user_id]
+        await c.send_message(user.id, 'Order "confirmed".')
+        print(user.id, user.first_name, "order confirmed")
--- plugins/start.py
+++ plugins/start.py
@@ -1,23 +1,36 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
+from pyrogram.enums import ChatType
 
 from locales import use_chat_lang
-
+from typing import Union
 
 @Client.on_message(filters.command("start"))
 @Client.on_callback_query(filters.regex(r"start_back"))
 @use_chat_lang()
-async def start(c, m, t):
-    keyboard = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text=t("inline_btn"), switch_inline_query_current_chat=""
-                ),
-                InlineKeyboardButton(text=t("settings_btn"), callback_data="settings"),
+async def start(c, m:Union[Message, CallbackQuery], t):
+    if not isinstance(m, filters.CallbackQuery) and m.chat.type != ChatType.PRIVATE:
+        keyboard = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(
+                        text=t("inline_btn"), switch_inline_query_current_chat=""
+                    ),
+                ]
             ]
-        ]
-    )
+        )
+    else:
+        keyboard = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(text=t("settings_btn"), callback_data="settings"),
+                    InlineKeyboardButton(text=t("donate_btn"), callback_data="donate")],[
+                    InlineKeyboardButton(
+                        text=t("inline_btn"), switch_inline_query_current_chat=""
+                    ),
+                ]
+            ]
+        )
     if isinstance(m, filters.CallbackQuery):
         await m.edit_message_text(text=t("start"), reply_markup=keyboard)
     else:
